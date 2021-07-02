560ab2c4ff6eda50fc1379c850dfa09ec51656b4
Celio Iyama <celio.iyama@gmail.com>
2021-07-02 17:32:50 -0300

    Membros estáticos - Parte 2

--- mebrosEstaticos1/src/util/Calculator.java
+++ mebrosEstaticos1/src/util/Calculator.java
@@ -1,12 +1,12 @@
 package util;
 
-public class Calculator {
-	public final double PI = 3.14159; // final -> constante PI em maiusculo toda constante
+public class Calculator { //quando eu coloco static nas funções então eu posso chamar direto a minha classe
+	public static final double PI = 3.14159; // final -> constante PI em maiusculo toda constante
 
-	public double circumference(double radius) {
+	public static double circumference(double radius) {
 		return 2 * PI * radius;
 	}
-	public double volume(double radius) {
+	public static double volume(double radius) {
 		return 4.0 * PI * radius * radius * radius / 3.0;
 	}
 }
