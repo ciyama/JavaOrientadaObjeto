66e7abaf371341548fcd403fdc441aff6159ddf5
Celio Iyama <celio.iyama@gmail.com>
2021-07-02 11:21:10 -0300

    Implementação dos metodos do triangulo no código

--- course/src/entities/Triangle.java
+++ course/src/entities/Triangle.java
@@ -5,4 +5,13 @@ public class Triangle {
 	public double a;
 	public double b;
 	public double c;
+	
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c)); // refatorando posso utilizar o return direto.
+		//double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		//return result;
+		
+		
+	}
 }
